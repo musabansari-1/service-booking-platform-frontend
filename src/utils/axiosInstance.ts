0a011c10c31d5951f8fb058de9db5aa66a8a1bfa
Musab Zahid Ansari <musabzahida@gmail.com>
2025-05-27 01:47:24 +0530

    fixed a typo in axiosInstance

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/api', // Set your base URL here
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL, // Set your base URL here
   headers: {
     'Content-Type': 'application/json',
   },
