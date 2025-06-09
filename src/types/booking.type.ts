a11d35c551b145ad694b12b1f45ba06157ae2c30
Musab Zahid Ansari <musabzahida@gmail.com>
2025-06-09 18:02:27 +0530

    Added Booking type

--- src/types/booking.type.ts
+++ src/types/booking.type.ts
@@ -1,3 +1,17 @@
+import {Service} from './service.types';
+import {Slot} from './slot.type';
+
 export interface BookingCardProps {
   serviceId: string;
 }
+
+export type Booking = {
+  _id: string;
+  userId: string;
+  serviceId: Service;
+  slotId: Slot;
+  status: 'pending' | 'confirmed' | 'canceled';
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
