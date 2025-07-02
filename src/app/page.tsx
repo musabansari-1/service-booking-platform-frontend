2cd6687e3db0efa5c1f9f15e7673f61e35265f93
Musab Zahid Ansari <musabzahida@gmail.com>
2025-07-02 17:33:00 +0530

    Added loading to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,35 +2,44 @@
 
 import ServiceCard from "@/components/ServiceCard";
 import { getAllServices } from "@/services/service";
-import Image from "next/image";
 import { useEffect, useState } from "react";
-import   { Service } from '@/types';
+import { Service } from '@/types';
 
 export default function Home() {
   const [services, setServices] = useState<Service[]>([]);
-  // const service = {
-  //   id: '66aa1494584635e15c073ca9',
-  //   name: 'Plumber',
-  //   description: 'From leaky faucets to emergency repairs, we offer reliable and efficient plumbing solutions. Our expert plumber ensures high-quality service with transparent pricing and prompt response. Trust us to handle all your plumbing needs with professionalism and care.',
-  //   imagePath: '/uploads/serviceImages/1721110956888-kouji-tsuru-xHeTZgvBfoM-unsplash.jpg',
-  //   price: '100',
-  //   duration: 3,
-  // }
+  const [loading, setLoading] = useState<boolean>(true);
 
   const fetchServices = async () => {
-    const response = await getAllServices();
-    setServices(response);
-  }
+    try {
+      const response = await getAllServices();
+      setServices(response);
+    } catch (err) {
+      console.error("❌ Failed to fetch services", err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     fetchServices();
-  },[])
+  }, []);
+
+  if (loading) {
+    return (
+      <main className="flex justify-center items-center h-screen">
+        <div className="text-center">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid mx-auto mb-4"></div>
+          <p className="text-lg font-semibold">Loading...</p>
+        </div>
+      </main>
+    );
+  }
 
   return (
     <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
-    {services.map((service: Service) => (
-      <ServiceCard key={service._id} service={service} />
-    ))}
-  </main>
+      {services.map((service: Service) => (
+        <ServiceCard key={service._id} service={service} />
+      ))}
+    </main>
   );
 }
